05e1a01251cd0afe0f40ac49cb48062b7c07421d
gruptwo2 <jope675@vidalibarraquer.net>
2022-12-12 11:53:43 +0000

    create game controller

--- controller/games-controller.js
+++ controller/games-controller.js
@@ -1,5 +1,21 @@
 const Games = require('../models/games-model');
 
 exports.createGame = (req,res) => {
+    const Games = require('../models/games-model');
 
+exports.createGame = async (req,res) => {
+    console.log(req.body.name)
+    const game = await Games.create({
+        createdBy: req.user._id,
+        name: req.body.name,
+        description: req.body.description,
+        linkoficialpage: req.body.linkoficialpage,
+        canyoubuyit: req.body.canyoubuyit,
+    })
+
+    res.status(200).json({
+        status: "succes",
+        game
+    });
+};
 };

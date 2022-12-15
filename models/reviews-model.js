9c997c758f2e22c0ff267cf3d16e42392d9509b9
gruptwo2 <jope675@vidalibarraquer.net>
2022-12-15 09:36:54 +0000

    reviews model

--- models/reviews-model.js
+++ models/reviews-model.js
@@ -1,1 +1,26 @@
 const mongoose = require('mongoose');
+
+const reviewsSchema = new mongoose.Schema({
+    createdBy: {
+        type: mongoose.ObjectId, // Object of user who post the review
+        required: true
+    },
+    commentedPostId: {
+        type: mongoose.ObjectId, // Object of user who post the review
+        required: true
+    },
+    text: {
+       required: true,
+       type: String,
+       minlenght: 1
+    },
+    stars:{
+        required: true,
+        min: 1,
+        max: 5
+    }
+});
+
+reviewsSchema.index({createdBy:-1,commentedPostId:-1},{unique: true});
+
+const Reviews = mongoose.model("reviews",reviewsSchema);
